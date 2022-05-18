9ae494e581ce286fe1804cbd1a457ebd192f2851
tkyg <80114433+tkyg@users.noreply.github.com>
2022-05-18 14:23:58 -0400

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const navLinks = links.map((link, index) => {
+    return <a key={index} href={`#${link}`}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{navLinks}</nav>;
 }
 
 export default NavBar;
